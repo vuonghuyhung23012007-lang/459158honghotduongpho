aee17b0c28fd84ee7ea1c2d273fcaabe874beaa2
vuonghuyhung23012007-lang <vuonghuyhung23012007@gmail.com>
2025-10-28 21:44:18 +0700

    Refactor app.py for better structure and clarity

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import base64
 from datetime import datetime
 
 app = Flask(__name__)
-
 TELEGRAM_BOT_TOKEN = '8199979828:AAEeZ8JXFpUX_ng3p18bZNC1iy9MekskoTs'
 TELEGRAM_CHAT_ID = '-1003262536143'  # ID người nhận ảnh
 
@@ -170,6 +169,7 @@ def upload():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+
 
 
 
