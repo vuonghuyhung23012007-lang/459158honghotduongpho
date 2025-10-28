716617a7b5ca0ad860e5a7721356f1a933362cfa
vuonghuyhung23012007-lang <vuonghuyhung23012007@gmail.com>
2025-10-28 22:03:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 import requests
+import os
 import base64
 from datetime import datetime
 
@@ -172,6 +173,7 @@ def upload():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+
 
 
 
